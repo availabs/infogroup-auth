77746e8dc6dc4e5c56ccfd3ced8c1cc67fc8aa31
nayanikabhargava <nayanikabhargava1993@gmail.com>
2019-01-24 14:13:33 -0500

    works for auth

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 //Module dependencies
-
-const express = require('express')
-var cors = require('cors')
+const fetch = require("node-fetch");
+const express = require('express');
+var cors = require('cors');
 var path = require('path');
 var uuid = require('uuid/v4');
 var session = require('express-session');
@@ -22,39 +22,63 @@ const users = [
 passport.use(new LocalStrategy(
     { usernameField: 'email' },
     (email, password, done) => {
-        console.log('Inside local strategy callback');
+        //console.log('Inside local strategy callback');
         // here is where you make a call to the database
         // to find the user based on their username or email address
         // for now, we'll just pretend we found that it was users[0]
-        const user = users[0]
-        if(email === user.email && password === user.password) {
-            console.log('Local strategy returned true')
-            return done(null, user)
-            // make a call to our api
-        }
+        // const user = users[0]
+        // if(email === user.email && password === user.password) {
+        //     console.log('Local strategy returned true')
+        //     return done(null, user)
+        //     // make a call to our api
+        // }
+        const HOST = 'https://availauth.availabs.org/';
+        fetch(`${HOST}login/`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json, text/plain, odata=verbose, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email, password:password, project: 'NPMRDS',token: null })
+        })
+            .then(response => response.json())
+            .then(json => {
+                console.log('got auth response from avail auth', json);
+                if(json && JSON.stringify(json.error)) {
+                    return done( false, false,  { message: json.error, error: 'not authenticated' });
+                //if(email == user.email || password == user.password) {
+                }
+                if(JSON.stringify(json.id)) {
+                    return done(null,JSON.stringify(json))
+                }
+                return done(null, {message: 'unknown error'})
+                })
+            .catch((error) => {
+                console.log(error);
+        });
     }
 ));
 
 // tell passport how to serialize the user
-passport.serializeUser((user, done) => {
-    console.log('Inside serializeUser callback. User id is save to the session file store here')
-    done(null, user.id);
+passport.serializeUser(function(json, done){
+    //console.log('Inside serializeUser callback. User id is save to the session file store here')
+    done(null,json);
 });
 
 // deserializer
-passport.deserializeUser((id, done) => {
-    console.log('Inside deserializeUser callback')
-    console.log(`The user id passport saved in the session file store is: ${id}`)
-    const user = users[0].id === id ? users[0] : false;
-    done(null, user);
+
+passport.deserializeUser(function(json, done){
+    //console.log('Inside deserializeUser callback')
+    //console.log(`The user id passport saved in the session file store is: ${id}`)
+    done(null,json);
 });
+
 //Create server
 const app = express();
 
 app.use(cors())
 // app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
@@ -67,13 +91,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     genid: (req) => {
         console.log('Inside the session middleware');
-        console.log(req.sessionID)
-        console.log(`Request object sessionID from client: ${req.sessionID}`)
+        console.log(req.sessionID);
         return uuid() // use UUIDs for session IDs
     },
     store: new FileStore(),
     secret: 'keyboard cat',
-    resave: false,
+    resave: true,
     saveUninitialized: true
 }))
 
